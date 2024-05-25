e173d2ec16bd44a6211aac4c57e639630057b26f
DSGog <dmitriy.amsec@gmail.com>
2024-05-25 22:01:21 +0400

    add some variables lesson1:task1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("HelloWorld");
+﻿// Console.WriteLine("HelloWorld");
+
+int value = 100;
+System.Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
